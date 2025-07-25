4ac0a4eca2c06229175bc1cbcb388c3cecf9b7c7
M  PAVAN KUMAR <166788144+starboypavan@users.noreply.github.com>
2025-07-25 07:11:24 +0530

    Add files via upload

---
+++ website v manage/script.js
@@ -0,0 +1,20 @@
+//functions to toglle text
+function toggleText1() {
+    //list1
+    const list1=document.getElementById("myList1").classList.toggle("open");
+}
+
+function toggleText2() {
+    //list2
+   const list1=document.getElementById("myList2").classList.toggle("open");
+}
+
+function toggleText3() {
+    //list3
+   const list1=document.getElementById("myList3").classList.toggle("open");
+}
+
+function toggleText4() {
+    //list4
+   const list1=document.getElementById("myList4").classList.toggle("open");
+}
